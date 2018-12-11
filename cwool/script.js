a5f1f5f356b2b46818e38989e915dda908db1d08
jfu103 <31708868+jfu103@users.noreply.github.com>
2018-12-11 06:44:50 -0800

    upload p3

--- cwool/script.js
+++ cwool/script.js
@@ -1,17 +1,42 @@
 let modimgs = [
 	
-	"/assets/01.jpg",
-	"/assets/02.jpg",
-	"/assets/03.jpg",
-	"/assets/04.jpg",
-	"/assets/05.jpg",
-	"/assets/06.jpg",
+	"/assets/00.png",
+	"/assets/01.png",
+	"/assets/02.png",
+	"/assets/03.png",
+	"/assets/04.png",
+	"/assets/05.png",
+	"/assets/06.png",
 	
 ];
 
 
-let r = "0"+Math.floor(Math.random() * modimgs.length)
-console.log(r)
-let mod1 = document.querySelector("#mod1")
+let r = "0"+Math.floor(Math.random() * modimgs.length);
+let r2 = "0"+Math.floor(Math.random() * modimgs.length);
+let r3 = "0"+Math.floor(Math.random() * modimgs.length);
+let r4 = "0"+Math.floor(Math.random() * modimgs.length);
+let r5 = "0"+Math.floor(Math.random() * modimgs.length);
+let r6 = "0"+Math.floor(Math.random() * modimgs.length);
 
-mod1.style.backgroundImage = `url(${[r]})`;
+
+console.log(r);
+//let mod1 = document.getElementById("mod1");
+
+
+
+document.getElementById('mod1').style.backgroundImage = 'url("/cwool/assets/'+r+'.png")';
+
+document.getElementById('mod2').style.backgroundImage = 'url("/cwool/assets/'+r2+'.png")';
+
+document.getElementById('mod3').style.backgroundImage = 'url("/cwool/assets/'+r3+'.png")';
+
+document.getElementById('mod4').style.backgroundImage = 'url("/cwool/assets/'+r4+'.png")';
+
+document.getElementById('mod5').style.backgroundImage = 'url("/cwool/assets/'+r5+'.png")';
+
+document.getElementById('mod6').style.backgroundImage = 'url("/cwool/assets/'+r6+'.png")';
+
+//$('#mod1').css('backgroundImage', [r]+'.jpg'); 
+
+//ADD THE FILE PATH CORRECTLY
+//FIX THE 
